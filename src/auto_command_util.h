9546f9749a348bd1ca16fbac38508bc5a84dfbbc
djdjdj62 <dj0976589080@gmail.com>
2023-06-05 23:20:34 +0800

    Update auto_command_util.h

--- src/auto_command_util.h
+++ src/auto_command_util.h
@@ -3,6 +3,7 @@
 #pragma once
 
 void pushButton(Button button, int delay_after_pushing_msec, int loop_num=1);
+void push2Button(Button button1, Button button2 , int delay_after_pushing_msec, int loop_num = 1);
 void pushHatButton(Hat button, int delay_after_pushing_msec, int loop_num=1);
 void pushHatButtonContinuous(Hat button, int pushing_time_msec);
 void tiltJoystick(int lx_per, int ly_per, int rx_per, int ry_per, int tilt_time_msec);
